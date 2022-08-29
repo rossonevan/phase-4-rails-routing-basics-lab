eefe272c4aae133aa322e81e98d1dd62ca5a1edb
Evan Rosson <rossonevan7281@gmail.com>
2022-08-29 10:17:52 -0500

    commit tree 1ce7399b80d0b8ccafcb1c1cfd9005cb8847ff94

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        students = Student.order(grade: :desc)
+        render json: students
+    end
+
+    def highest_grade
+        highest_grade_student = Student.order(grade: :desc).first
+        render json: highest_grade_student
+    end
+
+end
